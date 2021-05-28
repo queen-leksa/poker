a501ef7518549015a4d4f4f357bd3adcdf3b07fb
Leksa <surnacheva@ithub.ru>
2021-05-28 13:05:06 +0300

    Poker day2

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let hand = [];
-let cnt = 10;
+let cnt = 15;
 let el = document.querySelector(".cards");
+let text = document.querySelector(".result");
 
 for (let i = 0; i < cnt; i++) {
     hand.push(cards[i]);
@@ -8,3 +9,91 @@ for (let i = 0; i < cnt; i++) {
 }
 
 console.log(hand);
+/*
+    Старшая карта
+    1)Взять последовательность номиналов карт с конца
+    2)Взять колоду в руке
+    3) Начиная с самого последнего номинала (Туз) и заканчивая самой малнькой картой (6) Проверяем похожую карту в руке
+*/
+
+function hightCard(deck) {
+    for (let i = series.length - 1; i > 0; i--) {
+        for (let j = 0; j < deck.length; j++) {
+            if (series[i] === deck[j].rank) {
+                return deck[j];
+            }
+        }
+    }
+}
+
+function same(deck) {
+    let sameCards = {};
+    deck.forEach(card => {
+        if (sameCards[card.rank]) {
+            sameCards[card.rank]++;
+        } else {
+            sameCards[card.rank] = 1;
+        }
+    });
+    return sameCards;
+}
+function sameSuits(deck) {
+    let sameCards = {};
+    deck.forEach(card => {
+        // if (sameCards[card.suit]) {
+        //     sameCards[card.suit].push(card.rank);
+        // } else {
+        //     sameCards[card.suit] = [];
+        //     sameCards[card.suit].push(card.rank);
+        // }
+        if (!sameCards[card.suit]) {
+            sameCards[card.suit] = [];
+        }
+        sameCards[card.suit].push(card.rank);
+    });
+    return sameCards;
+}
+
+
+function getFour(same) {
+    for (let k in same) {
+        if (same[k] === 4) {
+            return k;
+        }
+    }
+}
+function getThree(same) {
+    for (let k in same) {
+        if (same[k] === 3) return k;
+    }
+}
+function getPairs(same) {
+    let pairs = [];
+    for (let k in same) {
+        if (same[k] === 2) pairs.push(k);
+    }
+    return pairs;
+}
+
+
+
+const ranksCnt = same(hand);
+
+let hight = hightCard(hand);
+let four = getFour(ranksCnt);
+let three = getThree(ranksCnt);
+let pairs = getPairs(ranksCnt);
+
+if (four) {
+    text.innerHTML = `Каре из ${four}`;
+} else if (three && pairs.length) {
+    text.innerHTML = `Фулл Хаус`;
+} else if (three) {
+    text.innerHTML = `Трипс из ${three}`;
+} else if (pairs.length) {
+    text.innerHTML = pairs.length === 2 ? `Две пары из ${pairs[0]} и ${pairs[1]}` : `Пара из ${pairs[0]}`;
+} else {
+    text.innerHTML = `Старшая карта:<br> ${hight.rank} ${hight.suit}`
+}
+
+console.log(sameSuits(hand));
